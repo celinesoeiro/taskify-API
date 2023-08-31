b2d4d0d1244ba5e1af4976a281790d65cee2b338
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-08-31 15:04:43 -0300

    feat/create server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log('aqui')
+import http from 'node:http';
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  console.log(method, url)
+})
+
+server.listen(3333);
